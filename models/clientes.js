f11c2b08ed7caeb6bf00d728b11b4b6145c2fd75
Andres Correa <jcorremo@everis.com>
2018-11-15 14:27:10 -0500

    model type

--- models/clientes.js
+++ models/clientes.js
@@ -1,13 +1,13 @@
 exports = module.exports = function(app, mongoose) {
 
 	var clienteSchema = new mongoose.Schema({ 
-		codigo: String, 
-   	identificador: String,  
-   	tipoIdentificador: String, 
-   	nombre: String,  
-   	tipoCliente: String,  
-   	codigoSolicitante: String,  
-   	razonSocial: String
+		codigo: { type: String }, 
+   	identificador: { type: String },  
+   	tipoIdentificador: { type: String }, 
+   	nombre: { type: String },  
+   	tipoCliente: { type: String },  
+   	codigoSolicitante: { type: String },  
+   	razonSocial: { type: String }
    });
 
 	mongoose.model('Customer', clienteSchema);
