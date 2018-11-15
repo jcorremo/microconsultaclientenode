8aaa91bc2aa8f7f7f13c00961fb826b05e24d78c
Andres Correa <jcorremo@everis.com>
2018-11-15 14:22:05 -0500

    customer controller

--- controllers/clientesController.js
+++ controllers/clientesController.js
@@ -1,13 +1,11 @@
 var mongoose = require('mongoose');
-var ClientesDB  = mongoose.model('Clientes');
+var ClientesDB  = mongoose.model('Customer');
 
 //GET - Return all tvshows in the DB
 exports.findAllClients = function(req, res) {
 	ClientesDB.find(function(err, clients) {
     if(err) res.send(500, err.message);
-
-    console.log('GET /clientes')
-		res.status(200).jsonp(clients);
+	res.status(200).jsonp(clients);
 	});
 };
 
