aab36030c303d901050a623d6dba99010cc7cf69
Andres Correa <jcorremo@everis.com>
2018-11-15 11:22:12 -0500

    ajuste DB

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express         = require("express"),
     mongoose        = require('mongoose');
 
 // Connection to DB
-mongoose.connect('mongodb://10.239.104.63/clientes', function(err, res) {
+mongoose.connect('mongodb://localhost/clientes', function(err, res) {
   if(err) throw err;
   console.log('Connected to Database');
 });
